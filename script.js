59d5512ed49523cecc5aa01901d16dd7ade928ee
christospap10 <xristospapotis@gmail.com>
2021-11-23 21:48:43 +0200

    clear fields

--- script.js
+++ script.js
@@ -85,9 +85,20 @@ var UIController = (function () {
       newHtml = newHtml.replace("%description%", obj.description);
       newHtml = newHtml.replace("%value%", obj.value);
       //insert the html into the DOM
-      document
-        .querySelector(DOMstrings.incomeContainer)
-        .insertAdjacentHTML("beforeend", newHtml);
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+    clearFields: function () {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function (current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus(); //set the cursor to the add description field
     },
     getDOMstrings: function () {
       return DOMstrings;
@@ -121,8 +132,10 @@ var controller = (function (budgetCtrl, UICtrl) {
     );
     //3. add the item to the UI
     UICtrl.addListItem(newItem, input.type);
-    //4. Calculate the budget
-    //5. Display the budget on the UI
+    //4. clear the fields
+    UICtrl.clearFields();
+    //5. Calculate the budget
+    //6. Display the budget on the UI
   };
   return {
     init: function () {
