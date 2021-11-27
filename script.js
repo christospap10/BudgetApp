21666183476257ee163e332ae516172d8385aeee
christospap10 <xristospapotis@gmail.com>
2021-11-27 20:43:23 +0200

    Update the UI

--- script.js
+++ script.js
@@ -88,6 +88,10 @@ var UIController = (function () {
     inputBtn: ".add__btn",
     incomeContainer: ".income__list",
     expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -131,6 +135,21 @@ var UIController = (function () {
 
       fieldsArr[0].focus(); //set the cursor to the add description field
     },
+
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          obj.percentage + "%";
+      } else {
+        document.querySelector(DOMstrings.budgetLabel).textContent =
+          "No available budget!";
+      }
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -156,7 +175,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     //2. Return the budget
     var budget = budgetCtrl.getBudget();
     //3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function () {
@@ -183,6 +202,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log("Application has started");
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1,
+      });
       setupEventListeners();
     },
   };
